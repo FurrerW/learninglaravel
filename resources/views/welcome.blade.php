025b7252f2c3931a05b8399602200dbd78ae7353
Walter Furrer <walterfurrer@proton.me>
2025-04-23 20:52:48 -0400

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/css/app.css')
+    <title>Learning Laravel</title>
+</head>
+<body class="bg-linear-to-b from-neutral-100 from-25% to-neutral-500 h-screen">
+    <header class="flex flex-col place-items-center justify-center h-96">
+        <h1 class="text-neutral-800 text-6xl font-black">Learning Laravel</h1>
+        <h2 class="text-rose-500 text-xl">Building a blog with Laravel so I can talk about learning Laravel</h2>
+    </header>
+</body>
+</html>
