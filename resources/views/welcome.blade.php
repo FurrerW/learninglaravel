f91115b07c76ad8c8dd682ba70c4c21e672c2498
Walter Furrer <walterfurrer@proton.me>
2025-04-24 13:09:31 -0400

    Added navbar and logo
    Some additional styling changes.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,19 @@
     @vite('resources/css/app.css')
     <title>Learning Laravel</title>
 </head>
-<body class="bg-linear-to-b from-neutral-100 from-25% to-neutral-500 h-screen">
-    <header class="flex flex-col place-items-center justify-center h-96">
-        <h1 class="text-neutral-800 text-6xl font-black">Learning Laravel</h1>
+<body class="bg-neutral-800 text-neutral-100 h-screen">
+    <nav class="flex flex-row justify-between items-center p-4 border-b-2 border-rose-500">
+        <ul class="">
+        <li><a href="/"><img src="{{ asset('images/logo-dark.jpg') }}" alt="logo" class="w-36"></a></li>
+        </ul>
+        <ul class="flex gap-x-4">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">Blog</a></li>
+            <li><a href="/contact">About</a></li>
+        </ul>
+    </nav>
+    <header class="flex flex-col place-items-center justify-center gap-y-2 h-96">
+        <h1 class=" text-6xl font-black">Learning Laravel</h1>
         <h2 class="text-rose-500 text-xl">Building a blog with Laravel so I can talk about learning Laravel</h2>
     </header>
 </body>
